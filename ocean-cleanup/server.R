c149f755f5fbd1311dbbc12af18f4766a4a2adc5
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2020-02-24 21:09:06 -0500

    latest changes to shiny

--- ocean-cleanup/server.R
+++ ocean-cleanup/server.R
@@ -1,27 +1,35 @@
+# install.packages("shiny")
+# install.packages("shinydashboard")
+# install.packages("shinythemes")
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
+
+
+
+# We'll save it in a variable `ui` so that we can preview it in the console
+ui <- fluidPage( theme="paper",
+                 
+    titlePanel("Ocean Cleanup"),
+    
+    sidebarLayout(
+      sidebarPanel(
+        textInput("txt", "Text input:", "text here"),
+        sliderInput("slider", "Slider input:", 1, 100, 30),
+        actionButton("action", "Button"),
+        actionButton("action2", "Button2", class = "btn-primary")
+      ),
+      
+      mainPanel(
+        box(title="test main", width="4")
+      )
+    )
+  
+)
 
 # Define server logic for random distribution application
-shinyServer(function(input, output) {
+server <- function(input, output) {
+  
+}
 
-  # Generate a plot of the data. Also uses the inputs to build the 
-  # plot label. Note that the dependencies on both the inputs and
-  # the data reactive expression are both tracked, and all expressions 
-  # are called in the sequence implied by the dependency graph
-  output$plot <- renderPlot({
-    dist <- input$dist
-    n <- input$n
-
-    hist(data(), 
-         main=paste('r', dist, '(', n, ')', sep=''))
-  })
-
-  # Generate a summary of the data
-  output$summary <- renderPrint({
-    summary(data())
-  })
-
-  # Generate an HTML table view of the data
-  output$table <- renderTable({
-    data.frame(x=data())
-  })
-})
+shinyApp(ui, server)
