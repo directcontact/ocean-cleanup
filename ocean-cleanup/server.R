42a6b66879544c0f2226cc0b627f3e779732c45f
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2020-02-25 00:09:08 -0500

    Added ui for the data xiuwei processed

--- ocean-cleanup/server.R
+++ ocean-cleanup/server.R
@@ -12,35 +12,44 @@
 # install.packages('readxl')
 # install.packages('usethis')
 # devtools::install_github("Displayr/flipAPI")
+# install.packages("DT")
 
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
+library(DT)
 source('D:/ocean-cleanup/ocean-cleanup/supporting-scripts/Agg_max_item_by_state.R')
 
 
 # We'll save it in a variable `ui` so that we can preview it in the console
 ui <- fluidPage( theme="paper",
-                 
-    titlePanel("Ocean Cleanup"),
     
+    titlePanel("Ocean Cleanup"),
+                 
     sidebarLayout(
-      sidebarPanel(
-        textInput("txt", "Text input:", "text here"),
-        sliderInput("slider", "Slider input:", 1, 100, 30),
-        actionButton("action", "Button"),
-        actionButton("action2", "Button2", class = "btn-primary")
-      ),
-      
+      sidebarPanel(),
       mainPanel(
+        tabsetPanel(
+          id = 'dataset',
+          tabPanel("watercraft", dataTableOutput("watercraft")),
+          tabPanel("land", dataTableOutput("land")),
+          tabPanel("underwater", dataTableOutput("underwater"))
+        )
       )
     )
-  
 )
 
 # Define server logic for random distribution application
 server <- function(input, output) {
-  
+  output$watercraft <- renderDataTable(
+    datatable(ocean.watercraft.maxitembystate)
+  )
+  output$land <- renderDataTable(
+    datatable(ocean.land.maxitembystate)
+  )
+  output$underwater <- renderDataTable(
+    datatable(ocean.underwater.maxitembystate)
+  )
 }
 
 shinyApp(ui, server)
